8a2a2f2add357b2966e73d9667853d8afbab6a66
nagahamaShinki <mail>
2026-09-18 11:57:52 +0900

    add 長濱 No01ログイン機能①ログイン画面への遷移の追加

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,9 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.WebDriver;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト ログイン機能①
@@ -18,6 +22,10 @@ import org.junit.jupiter.api.TestMethodOrder;
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース01 ログイン画面への遷移")
 public class Case01 {
+
+	private int port = 8080;
+
+	private WebDriver driver;
 
 	/** 前処理 */
 	@BeforeAll
@@ -35,7 +43,13 @@ public class Case01 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		driver = WebDriverUtils.webDriver;
+		WebDriverUtils.goTo("http://localhost:" + port + "/lms/");
+		String pageTitle = driver.getTitle();
+		assertEquals("ログイン | LMSaaa", pageTitle);
+		WebDriverUtils.getEvidence(new Object() {
+		}, "SUCCESS");
+
 	}
 
 }
