9294ab413390342066d426a861843ffdcc7c6a71
nagahamaShinki <mail>
2026-09-18 16:51:08 +0900

    add 長濱 ケース03 受講生 ログイン 正常系

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,11 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト ログイン機能①
@@ -18,6 +24,10 @@ import org.junit.jupiter.api.TestMethodOrder;
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース03 受講生 ログイン 正常系")
 public class Case03 {
+
+	private int port = 8080;
+
+	private WebDriver driver;
 
 	/** 前処理 */
 	@BeforeAll
@@ -35,14 +45,46 @@ public class Case03 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		driver = WebDriverUtils.webDriver;
+		goTo("http://localhost:" + port + "/lms/");
+		String pageTitle = driver.getTitle();
+		assertEquals("ログイン | LMS", pageTitle);
+
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		driver = WebDriverUtils.webDriver;
+		String loginText = "StudentAA01";
+		String passwordText = "StudentAA011";
+		//		final WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+
+		pageLoadTimeout(60);
+		// id password login
+		WebElement loginId = driver.findElement(By.name("loginId"));
+		loginId.clear();
+		loginId.sendKeys(loginText);
+
+		WebElement password = driver.findElement(By.name("password"));
+		password.clear();
+		password.sendKeys(passwordText);
+
+		visibilityTimeout(By.cssSelector("input[type='submit']"), 6);
+		WebElement loginBtn = driver.findElement(By.cssSelector("input[type='submit']"));
+		//		wait.until(ExpectedConditions.elementToBeClickable(loginBtn));
+		loginBtn.click();
+
+		visibilityTimeout(By.tagName("h2"), 6);
+		// assert
+		String pageTitle = driver.getTitle();
+		assertEquals("コース詳細 | LMS", pageTitle);
+
+		// screenshot
+		getEvidence(new Object() {
+		}, "SUCCESS");
+
 	}
 
 }
