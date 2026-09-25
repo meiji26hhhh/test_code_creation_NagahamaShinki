670018e1899c4f4b976ce8467aaacf5d465bb4fb
nagahamaShinki <mail>
2026-09-25 13:30:43 +0900

    add: 長濱 ケース07 テスト07 　テスト01 トップページURLでアクセス 　テスト02 初回ログイン済みの受講生ユーザーでログイン

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f03_report/Case07.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f03_report/Case07.java
@@ -1,14 +1,21 @@
 package jp.co.sss.lms.ct.f03_report;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト レポート機能
@@ -18,6 +25,10 @@ import org.junit.jupiter.api.TestMethodOrder;
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース07 受講生 レポート新規登録(日報) 正常系")
 public class Case07 {
+
+	private int port = 8080;
+
+	private WebDriver driver;
 
 	/** 前処理 */
 	@BeforeAll
@@ -31,18 +42,55 @@ public class Case07 {
 		closeDriver();
 	}
 
+	// 各メソッド実行前に都度、webDriver 準備する
+	@BeforeEach
+	void setUp() {
+		driver = WebDriverUtils.webDriver;
+	}
+
 	@Test
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:" + port + "/lms/");
+		String pageTitle = driver.getTitle();
+		assertEquals("ログイン | LMS", pageTitle);
+
+		// screenshot
+		getEvidence(new Object() {
+		}, "SUCCESS");
+
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		String loginText = "StudentAA01";
+		String passwordText = "StudentAA011";
+
+		pageLoadTimeout(60);
+		// id password login
+		WebElement loginId = driver.findElement(By.name("loginId"));
+		loginId.clear();
+		loginId.sendKeys(loginText);
+
+		WebElement password = driver.findElement(By.name("password"));
+		password.clear();
+		password.sendKeys(passwordText);
+
+		visibilityTimeout(By.cssSelector("input[type='submit']"), 6);
+		WebElement loginBtn = driver.findElement(By.cssSelector("input[type='submit']"));
+		loginBtn.click();
+
+		visibilityTimeout(By.tagName("h2"), 6);
+		// assert
+		String pageTitle = driver.getTitle();
+		assertEquals("コース詳細 | LMS", pageTitle);
+		// screenshot
+		getEvidence(new Object() {
+		}, "SUCCESS");
+
 	}
 
 	@Test
